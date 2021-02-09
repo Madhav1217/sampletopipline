7fe332ec97440af0bb322a0de44c208156059cdf
Madhav1217 <madhava1219@gmail.com>
2021-02-09 10:31:21 +0530

    added new files

--- varibles.tf
+++ varibles.tf
@@ -1,30 +1,3 @@
-variable "instance_name" {
-  description = "Used to populate the Name tag. This is done in main.tf"
-}
-
-variable "instance_type" {}
-
-variable "subnet_id" {
-  description = "The VPC subnet the instance(s) will go in"
-  default = "subnet-090e1a7308995684e"
-}
-
-variable "ami_id" {
-  description = "The AMI to use"
-  default = " ami-0d2751e39abf67ea8 (64-bit Arm"
-}
-
-variable "number_of_instances" {
-  description = "number of instances to make"
-  default = 1
-}
-
-variable "user_data" {
-  description = "The path to a file with user_data for the instances"
-}
-
-variable "tags" {
-  default = {
-    created_by = "terraform"
- }
-}
+variable "region" { default = "us-east-2" }
+variable "instance_type" { default = "t2.micro" }
+variable "key_name" { default = "madhav-poc" }
