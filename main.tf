332d7f581ce6c6b8a0b3e2b3f087777642113a8a
Madhav1217 <madhava1219@gmail.com>
2021-02-10 12:22:28 +0530

    main.tf file updated

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "webserver" {
-  ami             = "${data.aws_ami.ubuntu.id}"
+  ami             = "data.aws_ami.ubuntu.id"
   instance_type   = "${var.instance_type}"
   key_name        = "${var.key_name}"
   vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
